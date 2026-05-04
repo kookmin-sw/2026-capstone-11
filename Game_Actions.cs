75629ddf2d29dd56d0b65335a21a89e9e2a330c1
김세영 <65530747+EwhaWhiteMoon@users.noreply.github.com>
2026-05-04 14:19:48 +0900

    LastActionID

--- Game_Actions.cs
+++ Game_Actions.cs
@@ -5,6 +5,7 @@ namespace SeaEngine;
 
 public partial class Game
 {
+    public string LastActionId = "";
     private readonly UidFactory _actionUidFactory = new("A");
     private void UpdateActions()
     {
@@ -52,6 +53,7 @@ public partial class Game
 
     public void UseAction(Uid actionId)
     {
+        LastActionId = actionId.ToString();
         var selectedAction = _actions.FirstOrDefault(a => a.Guid == actionId) ?? throw new KeyNotFoundException($"No action with the guid : {actionId}");
         Logger.LogAction(selectedAction, Data);
         EffectRegistry.Get(selectedAction.EffectId).Apply(selectedAction.Source, selectedAction.Target, Data);
